f31fddf0462091ff56150dcd4109578155527186
IzStriker <zjac104@live.rhul.ac.uk>
2022-07-21 12:53:13 +0100

    feat: added migrations and seeding for account types

--- Models/ApplicationDbContext.cs
+++ Models/ApplicationDbContext.cs
@@ -7,6 +7,7 @@ namespace Backend.Models;
 public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
 {
     public DbSet<CustomerDetails>? CustomersDetails { get; set; }
+    public DbSet<AccountType>? AccountTypes { get; set; }
 
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> opt) : base(opt)
     { }
@@ -15,6 +16,37 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         base.OnModelCreating(builder);
         this.SeedRoles(builder);
+        this.SeedAccountTypes(builder);
+    }
+
+    /// <summary>
+    /// Create Customer Account Types
+    /// </summary>
+    private void SeedAccountTypes(ModelBuilder builder)
+    {
+        builder.Entity<AccountType>().HasData(
+            new AccountType()
+            {
+                Name = "Pay As You Go",
+                Description = "Pay for each rental before collection."
+            },
+            new AccountType()
+            {
+                Name = "Monthly",
+                Description = "Pay for rentals at the end of each month."
+            },
+            new AccountType()
+            {
+                Name = "Quarterly",
+                Description = "Pay for rentals every 3 months."
+            },
+            new AccountType()
+            {
+                Name = "Yearly",
+                Description = "Pay for rental at the end of the year."
+            }
+
+        );
     }
 
     /// <summary>
