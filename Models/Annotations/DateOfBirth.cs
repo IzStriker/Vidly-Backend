9dd36b3057aaacc4cae1b142a87a5d247800d252
IzStriker <zjac104@live.rhul.ac.uk>
2022-07-21 12:29:43 +0100

    fix: nullablility

--- Models/Annotations/DateOfBirth.cs
+++ Models/Annotations/DateOfBirth.cs
@@ -5,10 +5,8 @@ namespace Backend.Models.Annotations;
 public class DateOfBirth : ValidationAttribute
 {
 
-    public override bool IsValid(object value)
+    public override bool IsValid(object? value)
     {
-        if (value == null) return false;
-
         DateTime date = Convert.ToDateTime(value);
         return date.Date < DateTime.Now.Date;
     }
